39963b105ba1b2d012af769d4f286e9c13abed93
Lxh6688 <1272662790@qq.com>
2021-07-17 14:46:39 +0800

    request 和 responseeâ

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import * as http from "http";
+import {IncomingMessage, ServerResponse} from "http";
 
 const server = http.createServer();
 
-server.on('request', (request, response) => {
-  console.log('有人请求了')
-  response.end('hi')
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+  console.log('request.method')
+  console.log(request.method)
+  console.log('request.url')
+  console.log(request.url)
+  console.log('request.headers')
+  console.log(request.headers)
+  const array = []
+  request.on('data', (chunk) => {
+    array.push(chunk)
+  })
+  request.on('end', () => {
+    const body = Buffer.concat(array).toString()
+    response.end('hi')
+  })
+
 })
 
 server.listen(8888)

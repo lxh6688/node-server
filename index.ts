b087b6533032097ed029e1103a2cc4ec3759c02e
Lxh6688 <1272662790@qq.com>
2021-07-17 15:10:05 +0800

    根据 url 返回不同的文件

--- index.ts
+++ index.ts
@@ -1,24 +1,36 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as fs from "fs";
+import * as p from "path";
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname, 'public')
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  console.log('request.method')
-  console.log(request.method)
-  console.log('request.url')
-  console.log(request.url)
-  console.log('request.headers')
-  console.log(request.headers)
-  const array = []
-  request.on('data', (chunk) => {
-    array.push(chunk)
-  })
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString()
-    response.end('hi')
-  })
-
+  const {method, url, headers} = request
+  switch (url){
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html;charset-utf-8')
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error,data) => {
+        if(error) throw error;
+        response.end(data.toString())
+      })
+      break;
+      case '/style.css':
+        response.setHeader('Content-Type', 'text/css;charset-utf-8')
+        fs.readFile(p.resolve(publicDir, 'style.css'), (error,data) => {
+          if(error) throw error;
+          response.end(data.toString())
+        })
+        break;
+      case '/main.js':
+        response.setHeader('Content-Type', 'text/js;charset-utf-8')
+        fs.readFile(p.resolve(publicDir, 'main.js'), (error,data) => {
+          if(error) throw error;
+          response.end(data.toString())
+        })
+        break;
+  }
 })
 
 server.listen(8888)
